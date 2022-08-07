f48dff318e18b654e2115f387288461fc762c71b
Daniel Bento <danielbento_98@hotmail.com>
2022-08-07 12:11:28 +0100

    handle default channel for patchs notes

--- commands/removedefault.js
+++ commands/removedefault.js
@@ -3,7 +3,7 @@ const utils = require('../utils/utils');
 module.exports = {
     name: 'removedefault',
     short_name: 'remdef',
-    description: 'Removed the default channel. The bot will no longer notify your server of any patch notes',
+    description: 'Removes the default channel. The bot will no longer notify your server of any patch notes',
     arguments: '',
     adminOnly: true,
     showOnHelp: true,
@@ -14,19 +14,27 @@ module.exports = {
         }
         else {
              // Get all guilds that have set a default channel
-            const q = `SELECT * FROM Guilds WHERE guildID = ${message.guild.id} defaultChannelID IS NOT NULL LIMIT 1`
-            db.query(q, async (error, results) => {
-                if (error) {
-                    // TODO
-                    // tell the user that there was an error?
+            const q = `SELECT 1 FROM Guilds WHERE guildID = ${message.guild.id} AND defaultChannelID IS NOT NULL LIMIT 1`
+            db.query(q, async (select_error, select_results) => {
+                if (select_error) {
+                    utils.send_error_message(message, `There was an error while trying to remove the default channel`, 'send')
                 }
                 else {
-                    
-                    // TODO
-                    //if it is empty, tell the user a default is not set
-
-                    // else, try to change it to null. tell the user what happened..
-
+                    if (select_results.length === 0) {
+                        utils.send_success_message(message, `A default channel has not been set yet`, 'send')
+                    }
+                    else {
+                        // Replace guild's row
+                        const replace_q = `REPLACE INTO Guilds (guildID, guildOwnerID, defaultChannelID) VALUES(${message.guild.id}, ${message.guild.ownerId}, ${null})`
+                        db.query(replace_q, async (replace_error, replace_results) => {
+                            if (replace_error) {
+                                utils.send_error_message(message, `Failed to remove the default channel`, 'send')
+                            }
+                            else {
+                                utils.send_success_message(message, `Default channel has been successfully removed`, 'send')
+                            }
+                        });
+                    }
                 }
             })
         }
--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -1,3 +1,5 @@
+const utils = require('../../utils/utils');
+
 module.exports = (Discord, client, db, guild) => {
     
     console.log(`Joined guild ${guild.name}, id ${guild.id}`);
@@ -9,28 +11,56 @@ module.exports = (Discord, client, db, guild) => {
         channel.permissionsFor(guild.me).has('EMBED_LINKS')
     )
 
-    // TODO - add a selection menu for default channel for bot patch news
-    const default_channel = null
-
-    channel.send({
-        'content' : ' ',
-        'tts': false,
-        'embeds' : [{
-            'type' : 'rich',
-            'title': 'Just arrived',
-            'color' : 0x00FFFF,
-            'description': `Get a list of all my commands with **${process.env.MSG_PREFIX}help**`,
-        }]
-    })
-    .catch(msg_error => {
-        console.log(`ERROR :: could not send 'just arrived' message to channel ${channel.id} in guild ${guild.id}\n `, msg_error)
-    });
-
+    // Add to db.
+    // Default will be added when/if a channel is selected
     try {
         db.query(
-            `INSERT INTO Guilds VALUES (${guild.id}, ${guild.ownerId}, ${default_channel})`
+            `INSERT INTO Guilds VALUES (${guild.id}, ${guild.ownerId}, ${null})`
         );
     } catch (err) {
         console.log(`ERROR :: failed to insert guild ${guild.name} with id ${guild.id} into db\n `, err)
     }
+
+    guild.channels.fetch().then(channels => {
+        // Send a 'greetings' message and then send a 'select default' message
+        channel.send({
+            embeds : [{
+                'type' : 'rich',
+                'title': 'Just arrived',
+                'color' : 0x00FFFF,
+                'description': `Get a list of all my commands with **${process.env.MSG_PREFIX}help**`,
+            }]
+        })
+        .catch(msg_error => {
+            console.log(`ERROR :: could not send 'just arrived' message to channel ${channel.id} in guild ${guild.id}\n `, msg_error)
+        });
+
+        channel.send({
+            content : ' ',
+            embeds : [{
+                'type' : 'rich',
+                'title': `Select the channel you would like to set as default if you wish to be notified regarding patch notes.\nI can not see channels marked with ❌`,
+                'color' : 0x00FFFF,
+            }],
+            components: [{
+                'type': 1,
+                'components': [{
+                    "custom_id": `channel_select`,
+                    "placeholder": `Select default channel`,
+                    "options": utils.parse_channels_to_select_options(channels, guild),
+                    "min_values": 1,
+                    "max_values": 1,
+                    "type": 3
+                }]
+            }]
+        })
+        .then(arrival_msg => {
+            const setdefault = require('../../commands/setdefault')
+
+            setdefault.handleSelectDefault(client, db, guild, arrival_msg)
+        })
+        .catch(msg_error => {
+            console.log(`ERROR :: could not send 'select default' message after 'greetings' to channel ${channel.id} in guild ${guild.id}\n `, msg_error)
+        });
+    })
 }
